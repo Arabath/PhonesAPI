953665ac1e8a53a493d3bb582d273013552cb2d8
pmNiko <nikolas090189@gmail.com>
2022-09-16 18:04:43 -0300

    fix

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import morgan from "morgan";
-import phoneRoutes from "../routes/phone.routes";
+// import phoneRoutes from "../routes/phone.routes";
 
 const app = express()
 
@@ -8,14 +8,14 @@ const app = express()
 app.use(express.json())
 
 //Para solicitud de obj entrantes
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
 app.use(morgan('dev'))
 
-app.use( '/api/v1/phones', phoneRoutes )
+// app.use( '/api/v1/phones', phoneRoutes )
 
-app.use('/api/v1', (req, res, next)=> {
-    res.status(200).json({message: "Respuesta al navegador"})
+app.use('/api/v1', (req, res, next) => {
+    res.status(200).json({ message: "Respuesta al navegador" })
 })
 
 export default app;
